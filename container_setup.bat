780eb23fc3155e6c2938285fef93c1316b8f1d2e
Jon8RFC <Jon8RFC@users.noreply.github.com>
2024-01-12 22:52:30 -0600

    v1.1.1 container_setup.bat
    Docker Container setup for Windows, after Docker has been installed.

---
+++ container_setup.bat
@@ -0,0 +1,48 @@
+@echo off
+:name
+echo. & echo. & set /P name=Type a name in lowercase for the patcher and press ENTER:  
+if "%name%"=="" (
+echo. & echo. & echo           --- Please enter something as instructed ---
+goto:name
+)
+echo.
+echo -----
+echo Pruning the Docker builder...
+echo -----
+docker builder prune -f
+echo.
+echo.
+echo -----
+echo Building from Dockerfile...
+echo -----
+docker build --no-cache . -f Dockerfile_updated -t %name%/%name%:latest --squash
+echo.
+echo.
+echo -----
+echo Build complete.
+echo Running the container in detached mode...
+echo -----
+docker run -d -p 8000:8000 --name %name% %name%/%name%
+echo.
+echo.
+echo -----
+echo Stopping the container and setting always restart...
+echo -----
+docker stop %name%
+docker update --restart always %name%
+echo.
+echo.
+echo -----
+echo Pruning the Docker builder...
+echo -----
+docker builder prune -f
+echo.
+echo.
+echo -----
+echo Finished.
+echo.
+echo Go to Docker Containers and press the play button to start the container.
+echo -----
+echo.
+pause
+exit /b
