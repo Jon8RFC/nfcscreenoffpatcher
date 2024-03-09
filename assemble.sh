21037b671d637eb01b92afb98076df374d002bde
Jon8RFC <Jon8RFC@users.noreply.github.com>
2024-03-09 11:13:33 -0600

    v1.1.2

--- assemble.sh
+++ assemble.sh
@@ -3,5 +3,10 @@ EXTRACT_DIR=$1
 APK_NAME=$2
 
 cd "$EXTRACT_DIR/"
-apktool b -f "$APK_NAME/" -o "${APK_NAME}_mod.apk"
+SDK=$(grep '^SDK=' .env | cut -d'=' -f2)
+if [ "$SDK" -gt 33 ]; then
+	apktool_android14 b -f "$APK_NAME/" -o "${APK_NAME}_mod.apk"
+else
+	apktool_android13 b -f "$APK_NAME/" -o "${APK_NAME}_mod.apk"
+fi
 zipalign -v 4 "${APK_NAME}_mod.apk" "${APK_NAME}_align.apk"
