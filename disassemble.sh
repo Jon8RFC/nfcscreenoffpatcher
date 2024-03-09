5b8129d70e85ef5308426eed6d61e0f895a2605d
Jon8RFC <Jon8RFC@users.noreply.github.com>
2024-03-09 11:13:05 -0600

    v1.1.2

--- disassemble.sh
+++ disassemble.sh
@@ -3,5 +3,16 @@ EXTRACT_DIR=$1
 APK_NAME=$2
 
 cd "$EXTRACT_DIR/"
-apktool if framework-res.apk
-apktool d -f "$APK_NAME.apk" -o "$APK_NAME/"
+echo ""
+echo ""
+cat .env
+echo ""
+echo ""
+SDK=$(grep '^SDK=' .env | cut -d'=' -f2)
+if [ "$SDK" -gt 33 ]; then
+	apktool_android14 if framework-res.apk
+	apktool_android14 d -f "${APK_NAME}.apk" -o "${APK_NAME}/"
+else
+	apktool_android13 if framework-res.apk
+	apktool_android13 d -f "${APK_NAME}.apk" -o "${APK_NAME}/"
+fi
